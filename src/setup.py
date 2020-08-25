a45df4f764fd583ce7e293ca96823927cfb64d37
Marta Mauri <marta.mauri14@gmail.com>
2020-08-25 12:02:03 +0200

    modifying setup.py

--- src/setup.py
+++ src/setup.py
@@ -1,14 +1,8 @@
 import setuptools
-import os
 
 setuptools.setup(
     name="cost-functions",
-    version="0.1.0",
-    author="Marta Mauri",
-    author_email="marta.mauri@zapatacomputing.com",
-    description="Cost function package",
-    url="https://github.com/martamau/costs-users",
-    packages=setuptools.find_namespace_packages(),
+    packages=setuptools.find_packages(where="python"),
     package_dir={"": "python"},
     classifiers=(
         "Programming Language :: Python :: 3",
